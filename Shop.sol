8b230f76f771f86377efa21b1f407d01d4877ba2
Ilias Brinias <elliotpatra@mail.com>
2022-07-16 13:04:20 +0300

    Solidity contract completed. 1. store is the contract owner and he can accept and decline the transactions 2. modifiers added 3. fuction created for contract info

--- Shop.sol
+++ Shop.sol
@@ -4,6 +4,8 @@ import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract Shop is IERC20 {
     event E(uint256 msg);
+    event TokenInfo(string name, string symbol, uint256 decimals, uint256 totalSupply, uint256 ethereumExchangeRate);
+    event ContractInfo(address store, address[3] admins, uint256 balance, uint256 pendingBalance);
 
     string PENDING_REQ = "Pending";
     string ACCEPT_REQ = "Accept";
@@ -11,45 +13,42 @@ contract Shop is IERC20 {
     string CLIENT_DECLINE_REQ = "User Decline";
 
     // public data
-    address public owner;                  // owner 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
-    address[3] public admins;              // customers EOA
-    uint256 public balance;                // balance
-    uint256 public pendingBalance;         // balance from Pending Transactions
-    Transaction[] public acceptedTransactions;
-    Transaction[] public declinedTransactions;
-    Transaction[] public clientDeclinedTransactions;
-    WithdrawRequest[] public withdrawAccepted;
-    WithdrawRequest[] public withdrawDeclined;
+    address store;                  // owner 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+    address[3] admins;              // customers EOA
+    uint256 balance;                // balance
+    uint256 pendingBalance;         // balance from Pending Transactions
+    Transaction[] acceptedTransactions;
+    Transaction[] declinedTransactions;
+    Transaction[] clientDeclinedTransactions;
+    WithdrawRequest[] withdrawAccepted;
+    WithdrawRequest[] withdrawDeclined;
 
     // ERC-20 Token properties
-    string public name = "MyShopToken";
-    string public symbol = "MST";
-    uint256 public decimals = 18;
-    uint256 public totalSupply_ = 1000000000000000000000000; // 1,000,000 + 18 decimals
-    uint256 public ethereumExchangeRate = 60; // 1 wei = 60 MST
+    string name = "MyShopToken";
+    string symbol = "MST";
+    uint256 decimals = 18;
+    uint256 totalSupply_ = 1000000000000000000000000; // 1,000,000 + 18 decimals
+    uint256 ethereumExchangeRate = 60; // 1 wei = 60 MST
     mapping(address => uint256) balances;
     mapping(address => mapping (address => uint256)) allowed;
 
     // private
-    address[] pendingTransaction;
+    uint256[] pendingTransaction;
     mapping(address=>address[]) answeredTransactions;
     mapping(address => bool) adminExists;
     mapping(address => uint256) adminIdx;
-    mapping(address => Transaction) transactionMap;
+    mapping(uint256 => Transaction) transactionMap;
     AdminAccountChange public adminAccountChange;
     WithdrawRequest public withdrawRequest;
     uint256 withdrawId = 0;
-
+    uint256 idTransaction = 0;
     // structs
     struct Transaction{
         uint256 productId;
         address from;
         uint256 amount;
         string state;
-        uint accept;
-        uint rejected;
         bool exists;
-        address[] answeredAdmins;
     }
 
     struct AdminAccountChange{
@@ -74,7 +73,7 @@ contract Shop is IERC20 {
 
     constructor() {
         // initialize the owner and 3 admins
-        owner = msg.sender;
+        store = msg.sender;
         admins[0] = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
         adminExists[admins[0]] = true;
         adminIdx[admins[0]] = 0;
@@ -91,7 +90,8 @@ contract Shop is IERC20 {
         balances[msg.sender] = totalSupply_;
     }
     // ----
-    // Withdraws
+
+    // Modifiers
     modifier withdrawCheck(){
         // check if he is admin
         require(adminExists[msg.sender], "You must be admin to accept this Withdraw");
@@ -105,6 +105,14 @@ contract Shop is IERC20 {
         }
         _;
     }
+    modifier onlyStore() {
+        // check if he is admin
+        require(store == msg.sender, "You must be owner");
+        _;
+    }
+    // ----
+
+    // Withdraw
     function withdraw(uint256 tokenAmount) public returns (uint256 idWithdrawRequest){
         require(tokenAmount <= balance, "The contract does not have the number of tokens you want to withdraw.");
 
@@ -179,6 +187,7 @@ contract Shop is IERC20 {
         return "The Withdraw still Pending";
     }
     // ----
+
     // return all the Pending Transactios
     function showPendingTransactions() public view returns(Transaction[] memory){
         Transaction[] memory t = new Transaction[](pendingTransaction.length);
@@ -188,134 +197,97 @@ contract Shop is IERC20 {
         return (t);
     }
     // Transaction create accept decline
-    function createTransaction(uint idProduct, uint amountToken) public {
+    function createTransaction(uint amountToken) public returns (uint256){
         require(balances[msg.sender]>=amountToken, "You don't have enough tokens for this Transaction");
         // check if the amount is bigger than the product amount
 
-        // check if the transaction exists
-        require(!transactionMap[msg.sender].exists, "You have allready a pending transaction, decline the old one first");
-
+        idTransaction+=1;
         // save the transaction as pending
         pendingBalance += amountToken;
         balances[msg.sender] -= amountToken;
 
         // save the Transaction
-        pendingTransaction.push(msg.sender);
-        address[] memory a;
-        transactionMap[msg.sender] = Transaction(
-            idProduct,
+        pendingTransaction.push(idTransaction);
+        transactionMap[idTransaction] = Transaction(
+            idTransaction,
             msg.sender,
             amountToken,
             PENDING_REQ,
-            0,
-            0,
-            true,
-            a
+            true
         );
         emit E(amountToken);
+        return(idTransaction);
     }
-    function acceptTransaction(address clientAddress) public returns(string memory){
-        // check if he is admin
-        require(adminExists[msg.sender], "You must be admin to accept this Transaction");
+    function acceptTransaction(uint256 id) onlyStore public returns(string memory){
         // check if the transaction exists
-        require(transactionMap[clientAddress].exists, "This Transaction doenst exist");
+        require(transactionMap[id].exists, "This Transaction doenst exist");
         // check if the transaction pending
-        require(keccak256(bytes(transactionMap[clientAddress].state))==keccak256(bytes(PENDING_REQ)), "This Transaction isn't pending");
+        require(keccak256(bytes(transactionMap[id].state))==keccak256(bytes(PENDING_REQ)), "This Transaction isn't pending");
 
-        // check if the admin is aswered allready
-        address[] storage answered = transactionMap[clientAddress].answeredAdmins;
-        for(uint i=0; i<answered.length;i++){
-            require(answered[i] != msg.sender, "You have aswered allready this transaction");
-        }
-        // push the
-        answered.push(msg.sender);
-        transactionMap[clientAddress].answeredAdmins = answered;
-        transactionMap[clientAddress].accept+=1;
-
-        if(transactionMap[clientAddress].accept > 1){
-            // make the Transaction accepted
-            transactionMap[clientAddress].state = ACCEPT_REQ;
-
-            // find the index of the address in the pendingTransaction array and delete it
-            for(uint i=0; i<pendingTransaction.length;i++){
-                if(pendingTransaction[i] == clientAddress){
-                    // delete Pending Transactions
-                    pendingTransaction = deletePendingTransaction(i);
-                    // add to contract the amount of accepted Transaction
-                    balance+=transactionMap[clientAddress].amount;
-                    pendingBalance-=transactionMap[clientAddress].amount;
-                    acceptedTransactions.push(transactionMap[clientAddress]);
-                    delete transactionMap[msg.sender];
-                    break;
-                }
-            }
-            return "The Transaction is accepted";
-        }
-
-        return "The Transaction still Pending";
-    }
-    function declineYourTransaction() public returns(string memory){
-        // check if the transaction exists
-        require(transactionMap[msg.sender].exists, "You dont have any Transactions");
-        // check if the transaction pending
-        require(keccak256(bytes(transactionMap[msg.sender].state))==keccak256(bytes(PENDING_REQ)), "Your Transaction isn't pending");
-
-        // make the Transaction declined
-        transactionMap[msg.sender].state = CLIENT_DECLINE_REQ;
+        // make the Transaction accepted
+        transactionMap[id].state = ACCEPT_REQ;
 
         // find the index of the address in the pendingTransaction array and delete it
         for(uint i=0; i<pendingTransaction.length;i++){
-            if(pendingTransaction[i] == msg.sender){
+            if(pendingTransaction[i] == id){
+                // delete Pending Transactions
                 pendingTransaction = deletePendingTransaction(i);
-                pendingBalance -= transactionMap[msg.sender].amount;
-                clientDeclinedTransactions.push(transactionMap[msg.sender]);
+                // add to contract the amount of accepted Transaction
+                balance+=transactionMap[id].amount;
+                pendingBalance-=transactionMap[id].amount;
+                acceptedTransactions.push(transactionMap[id]);
+                delete transactionMap[id];
+                break;
+            }
+        }
+        return "The Transaction is accepted";
+    }
+    function declineYourTransaction(uint256 id) public returns(string memory){
+        // check if the transaction exists
+        require(transactionMap[id].exists, "You dont have any Transactions");
+        // check if the transaction pending
+        require(keccak256(bytes(transactionMap[id].state))==keccak256(bytes(PENDING_REQ)), "Your Transaction isn't pending");
+
+        // make the Transaction declined
+        transactionMap[id].state = CLIENT_DECLINE_REQ;
+
+        // find the index of the address in the pendingTransaction array and delete it
+        for(uint i=0; i<pendingTransaction.length;i++){
+            if(pendingTransaction[i] == id){
+                pendingTransaction = deletePendingTransaction(i);
+                pendingBalance -= transactionMap[id].amount;
+                clientDeclinedTransactions.push(transactionMap[id]);
                 //return the amount cost of the product
-                //(payable (msg.sender)).transfer(transactionMap[msg.sender].amount);
-                balances[msg.sender] += transactionMap[msg.sender].amount;
-                delete transactionMap[msg.sender];
+                balances[msg.sender] += transactionMap[id].amount;
+                delete transactionMap[id];
                 return "Your Transaction declined successfully";
             }
         }
 
         return "Something Happened";
     }
-    function declineTransaction(address payable clientAddress) public returns(string memory){
-        // check if he is admin
-        require(adminExists[msg.sender], "You must be admin to accept this Transaction");
+    function declineTransaction(uint256 id) onlyStore public returns(string memory){
         // check if the transaction exists
-        require(transactionMap[clientAddress].exists, "This Transaction doenst exist");
+        require(transactionMap[id].exists, "This Transaction doenst exist");
         // check if the transaction pending
-        require(keccak256(bytes(transactionMap[clientAddress].state))==keccak256(bytes(PENDING_REQ)), "This Transaction isn't pending");
+        require(keccak256(bytes(transactionMap[id].state))==keccak256(bytes(PENDING_REQ)), "This Transaction isn't pending");
 
-        // check if the admin is aswered allready
-        address[] storage answered = transactionMap[clientAddress].answeredAdmins;
-        for(uint i=0; i<answered.length;i++){
-            require(answered[i] != msg.sender, "You have aswered allready this transaction");
-        }
-        // push the
-        answered.push(msg.sender);
-        transactionMap[clientAddress].answeredAdmins = answered;
-        transactionMap[clientAddress].rejected+=1;
+        // make the Transaction declined
+        transactionMap[id].state = DECLINE_REQ;
 
-        if(transactionMap[clientAddress].rejected > 1){
-            // make the Transaction declined
-            transactionMap[clientAddress].state = DECLINE_REQ;
-
-
-            // find the index of the address in the pendingTransaction array and delete it
-            for(uint i=0; i<pendingTransaction.length;i++){
-                if(pendingTransaction[i] == clientAddress){
-                    pendingTransaction = deletePendingTransaction(i);
-                    pendingBalance -= transactionMap[clientAddress].amount;
-                    declinedTransactions.push(transactionMap[clientAddress]);
-                    clientAddress.transfer(transactionMap[clientAddress].amount);
-                    delete transactionMap[clientAddress];
-                    break;
-                }
+        // find the index of the address in the pendingTransaction array and delete it
+        for(uint i=0; i<pendingTransaction.length;i++){
+            if(pendingTransaction[i] == id){
+                pendingTransaction = deletePendingTransaction(i);
+                pendingBalance -= transactionMap[id].amount;
+                declinedTransactions.push(transactionMap[id]);
+                //return the amount cost of the product
+                balances[msg.sender] += transactionMap[id].amount;
+                delete transactionMap[id];
+                break;
             }
-            return "The Transaction is declined and the amount of the product is returned to the client";
         }
-        return "The Transaction still Pending";
+        return "The Transaction is declined and the amount of the product is returned to the client";
     }
     // -- -- --
 
@@ -390,7 +362,7 @@ contract Shop is IERC20 {
     }
     // -- -- --
     function destroy() private{
-        selfdestruct(payable(owner));
+        selfdestruct(payable(store));
     }
     // -- -- --
     // Get value and data from the client
@@ -401,7 +373,7 @@ contract Shop is IERC20 {
     // -- -- --
     // Move the last element to the deleted spot.
     // Remove the last element.
-    function deletePendingTransaction(uint index) internal returns(address[] storage) {
+    function deletePendingTransaction(uint index) internal returns(uint256[] storage) {
         require(index < pendingTransaction.length);
         pendingTransaction[index] = pendingTransaction[pendingTransaction.length-1];
         pendingTransaction.pop();
@@ -456,4 +428,30 @@ contract Shop is IERC20 {
         delete balances[msg.sender];
         return etherAmount;
     }
+    // ----
+
+    // ----
+    // rest contract info
+    function showAcceptTransactions() public view returns(Transaction[] memory){
+        return acceptedTransactions;
+    }
+    function showDeclineTransactions() public view returns(Transaction[] memory){
+        return declinedTransactions;
+    }
+    function showClientDeclinedTransactions() public view returns(Transaction[] memory){
+        return clientDeclinedTransactions;
+    }
+    function showAcceptedWithdraws() public view returns(WithdrawRequest[] memory){
+        return withdrawAccepted;
+    }
+    function showDeclinedWithdraws() public view returns(WithdrawRequest[] memory){
+        return withdrawDeclined;
+    }
+    function showTokenInfo() public{
+        emit TokenInfo(name,symbol,decimals,totalSupply_,ethereumExchangeRate);
+    }
+    function showContractInfo() public{
+        emit ContractInfo(store,admins,balance,pendingBalance);
+    }
+
 }
